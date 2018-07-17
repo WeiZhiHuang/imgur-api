08eb36dbec3c92c104cb612bf4aebb3a56906308
黃威智 <huangweizhi@huangweizhide-MacBook-Pro.local>
2018-07-17 17:52:52 +0800

    add upload feature

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,55 @@
 <template>
-  <div>Hello, world!</div>
+  <div>
+    <input type="file" @change="onChange" accept="image/*">
+    <button v-if="image" @click="upload">Upload</button>
+    <div v-show="url">
+      <a :href="url" target="_blank">{{ url }}</a>
+    </div>
+    <div v-show="image">
+      <img :src="image">
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'app',
+  data() {
+    return {
+      image: null,
+      url: null,
+    };
+  },
+  methods: {
+    onChange() {
+      this.image = this.url = null;
+      this.previewImage(event);
+    },
+    previewImage(e) {
+      const input = e.target.files[0];
+      if (input) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.image = event.target.result;
+        };
+        reader.readAsDataURL(input);
+      }
+    },
+    upload() {
+      axios
+        .post(
+          'https://api.imgur.com/3/image',
+          { image: this.image.split(',')[1] },
+          {
+            headers: { Authorization: 'Client-ID 14fd0da0bdb0160' },
+          },
+        )
+        .then(({ data }) => {
+          this.url = data.data.link;
+        });
+    },
+  },
 };
 </script>
